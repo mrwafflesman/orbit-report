6de20912b9186639ca6e786d5702ab57af3a4511
Nevin Barker <nbarker@dgervargiziangmail.onmicrosoft.com>
2020-09-14 15:31:44 -0500

    up to step six

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Satellite } from './satellite';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor() {
+    this.sourceList = [];
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+
+    window.fetch(satellitesUrl).then(function(response) {
+      response.json().then(function(data) {
+
+         let fetchedSatellites = data.satellites;
+         for (let i=0; i<fetchedSatellites.length; i++) {
+            let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+            this.sourceList.push(satellite)
+         }
+
+      }.bind(this));
+    }.bind(this));
+  }
+  sourceList: Satellite[];
   title = 'orbit-report';
 }
