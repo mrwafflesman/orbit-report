dbdd0db579c4b3b00ae21e4d9eb9ac216913cf69
Nevin Barker <nbarker@dgervargiziangmail.onmicrosoft.com>
2020-09-16 19:09:53 -0500

    done plus two bonus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ import { Satellite } from './satellite';
 export class AppComponent {
   constructor() {
     this.sourceList = [];
+    this.displayList = [];
     let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
 
     window.fetch(satellitesUrl).then(function(response) {
@@ -19,10 +20,24 @@ export class AppComponent {
             let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
             this.sourceList.push(satellite)
          }
-
+         this.displayList = this.sourceList.slice(0);
       }.bind(this));
     }.bind(this));
   }
+
+  search(searchTerm: string): void {
+    let matchingSatellites: Satellite[] = [];
+    searchTerm = searchTerm.toLowerCase();
+    for(let i=0; i < this.sourceList.length; i++) {
+       let name = this.sourceList[i].name.toLowerCase();
+       if (name.indexOf(searchTerm) >= 0) {
+          matchingSatellites.push(this.sourceList[i]);
+       }
+    }
+    this.displayList = matchingSatellites;
+ }
+
   sourceList: Satellite[];
+  displayList: Satellite[];
   title = 'orbit-report';
 }
