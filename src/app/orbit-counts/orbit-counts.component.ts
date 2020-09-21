6f505980d395b04f0a5d12d4b357abed1d9fc5e5
Nevin Barker <nbarker@dgervargiziangmail.onmicrosoft.com>
2020-09-21 15:51:56 -0500

    done

--- src/app/orbit-counts/orbit-counts.component.ts
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -15,4 +15,50 @@ export class OrbitCountsComponent implements OnInit {
   ngOnInit() {
   }
 
+  getSum(whatSum: string): number {
+    let count = 0
+    if (whatSum === 'total') {
+      for (let i=0; i<this.satellites.length; i++) {
+        count++
+      }
+    } else if (whatSum.toLowerCase() === 'space debris') {
+      for (let i=0; i<this.satellites.length; i++) {
+        if (this.satellites[i]['type'].toLowerCase() === 'space debris') {
+          count++
+        }
+      }
+    } else if (whatSum.toLowerCase() === 'communication') {
+      for (let i=0; i<this.satellites.length; i++) {
+        if (this.satellites[i]['type'].toLowerCase() === 'communication') {
+          count++
+        }
+      }
+    } else if (whatSum.toLowerCase() === 'probe') {
+      for (let i=0; i<this.satellites.length; i++) {
+        if (this.satellites[i]['type'].toLowerCase() === 'probe') {
+          count++
+        }
+      }
+    } else if (whatSum.toLowerCase() === 'positioning') {
+      for (let i=0; i<this.satellites.length; i++) {
+        if (this.satellites[i]['type'].toLowerCase() === 'positioning') {
+          count++
+        }
+      }
+    } else if (whatSum.toLowerCase() === 'space station') {
+      for (let i=0; i<this.satellites.length; i++) {
+        if (this.satellites[i]['type'].toLowerCase() === 'space station') {
+          count++
+        }
+      }
+    } else if (whatSum.toLowerCase() === 'telescope') {
+      for (let i=0; i<this.satellites.length; i++) {
+        if (this.satellites[i]['type'].toLowerCase() === 'telescope') {
+          count++
+        }
+      }
+    }
+    return count
+  }
+
 }
